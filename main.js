17940278c8d08c5d9cf2868f4ee2aed16df452b4
Ivan Kozhokar <dmb14suka@gmail.com>
2021-11-13 23:09:43 -0800

    finnished gallery

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+var btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
 
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
-
+/*  Looping through images */
+for(let i = 1; i<=5; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  newImage.onclick = function(e) {
+  	let attribute = e.target.getAttribute('src');
+  	display(attribute);
+  }
+  function display(attribute){
+  	displayedImage.setAttribute('src', attribute);
+  }
+  thumbBar.appendChild(newImage);
+}
 /* Wiring up the Darken/Lighten button */
+
+btn.onclick = function() {
+	let btnCheck = btn.getAttribute('class');
+	if(btnCheck === 'dark'){
+		btn.setAttribute('class', 'light');
+		btn.textContent = 'Lighten';
+		overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+	}
+	else{
+		btn.setAttribute('class', 'dark');
+		btn.textContent = 'Darken';
+		overlay.style.backgroundColor = 'rgba(0,0,0,0)';	
+	}
+}
+
